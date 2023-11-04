d8b456df403b88aa2553ce27e742abc74de0d99f
Nandika-A <nandikaagrawal610@gmail.com>
2023-11-04 23:36:06 +0530

    added events and savings

--- contracts/categories.sol
+++ contracts/categories.sol
@@ -5,6 +5,8 @@ import "./ownable.sol";
 
 contract Category is Ownable {
 
+    event NewTransaction(uint id);
+
     // structure to store transaction details
     struct Transaction {
         address sender;
@@ -17,12 +19,16 @@ contract Category is Ownable {
     mapping(uint => string) transactionToCategory;
     mapping(string => uint) transactionsPerCategory;
 
+    uint totalTransactions;
+
     // Record Transaction Function
-    function recordTransaction(address _sender, address _receiver, uint _amount, uint _timestamp, string memory _category) public onlyOwner {
-        transactions.push(Transaction(_sender, _receiver, _amount, _timestamp));
+    function recordTransaction(address _sender, address _receiver, uint _amount, string memory _category) public onlyOwner {
+        transactions.push(Transaction(_sender, _receiver, _amount, block.timestamp));
         uint id = transactions.length - 1;
         transactionToCategory[id] = _category;
         transactionsPerCategory[_category]++;
+        totalTransactions++;
+        emit NewTransaction(id);
     }
 
     // Get transactions
@@ -38,6 +44,43 @@ contract Category is Ownable {
             }
         }
         return result;
+    }
+
+    function getTransactionsByTime(uint _timeperiod) public view onlyOwner returns(uint[] memory) {
+        uint[] memory result = new uint[](totalTransactions);
+        uint counter = 0;
+        for(uint i = 0; i < transactions.length; i++)
+        {
+            if(transactions[i].timestamp >= (block.timestamp - _timeperiod))
+            {
+                result[counter] = i;
+                counter ++;
+            }
+        }
+        return result;
+    } 
+
+    function getTotalExpenses(uint _timeperiod) public view onlyOwner returns(
+        uint earnings,
+        uint expenditure,
+        uint savings
+    ) {
+        uint[] memory transactionsInTime = getTransactionsByTime(_timeperiod);
+        uint t_earnings = 0;
+        uint t_savings = 0;
+        uint t_expenditure = 0;
+        for(uint i = 0; i < transactionsInTime.length; i++)
+        {
+            if(transactions[transactionsInTime[i]].sender == owner())
+                t_expenditure += transactions[transactionsInTime[i]].amount;
+
+            else if(transactions[transactionsInTime[i]].receiver == owner())
+                t_earnings += transactions[transactionsInTime[i]].amount;
+            
+        }
+        t_savings = t_earnings - t_expenditure;
+        
+        return (t_earnings, t_expenditure, t_savings);
     }
 
 }
