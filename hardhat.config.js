ec58b43babdfead078efc17f9b1c70855d0348a6
Nandika-A <nandikaagrawal610@gmail.com>
2023-11-05 15:50:19 +0530

    deployed categories

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_RPC_URL =
+  process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY =
+  process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
 module.exports = {
   solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+      gas: 2100000,
+      gasPrice: 8000000000,
+      saveDeployments: true,
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,47 @@
-const hre = require("hardhat");
-const { ethers } = require("hardhat");
+// imports
+const { ethers, run, network } = require("hardhat")
 
+// async main
 async function main() {
-  
+  const storageFactory = await ethers.getContractFactory("Category")
+  console.log("Deploying contract...")
+  const category = await storageFactory.deploy()
+  console.log(`Deployed contract to: ${category.target}`)
+
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block confirmations...")
+    await category.deployTransaction.wait(6)
+    await verify(category.address, [])
+  }
+
+  // Record transaction
+  const transactionResponse = await category.recordTransaction("0xe2BA10C388ef4A013Db4ff13f56B742893208D05", "0xe2BA10C388ef4A013Db4ff13f56B742893208D05", 125, "food");
+  await transactionResponse.wait(1)
+  const updatedValue = await category.getTransactionsByCategory("food")
+  console.log(`Updated Value is: ${updatedValue}`)
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// async function verify(contractAddress, args) {
+const verify = async (contractAddress, args) => {
+  console.log("Verifying contract...")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!")
+    } else {
+      console.log(e)
+    }
+  }
+}
+
+// main
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
